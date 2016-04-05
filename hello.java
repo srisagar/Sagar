b26ba604d8e1513a1acf6ff171e781379d7a9777
Sagar <hexstu006@sdettraining.com>
2016-04-05 10:20:43 -0400

    Hello Java File Created

--- hello.java
+++ hello.java
@@ -9,7 +9,5 @@ class Hello
     public void Hello1()    {
         System.out.println( "Hi from git to jenkins" );
     }
-public static void main(String args[])   {
-        new Hello().Hello1();
-    }
+
 }
